2b238c01fe038e74bcbaf211532d4aa625ee0ccb
vlkvkn <140600886+vlkvkn@users.noreply.github.com>
2025-05-27 19:17:01 +0000

    Try to improve bugs

--- src/hooks/useCounterContract.ts
+++ src/hooks/useCounterContract.ts
@@ -2,16 +2,20 @@ import { useEffect, useState } from 'react';
 import Counter from '../contracts/counter';
 import { useTonClient } from './useTonClient';
 import { useAsyncInitialize } from './useAsyncInitialize';
+import { useTonConnect } from './useTonConnect';
 import { Address, OpenedContract } from '@ton/core';
 
 export function useCounterContract() {
   const client = useTonClient();
-  const [val, setVal] = useState<null | number>();
+  const [val, setVal] = useState<null | string>();
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
 
   const counterContract = useAsyncInitialize(async () => {
     if (!client) return;
     const contract = new Counter(
-      Address.parse('EQCRR87StVniASzvZVfV_GLkbk8PCQyem4QsMigiaQ2UHYko')
+      Address.parse('EQCRR87StVniASzvZVfV_GLkbk8PCQyem4QsMigiaQ2UHYko') // replace with your address from tutorial 2 step 8
     );
     return client.open(contract) as OpenedContract<Counter>;
   }, [client]);
@@ -21,7 +25,9 @@ export function useCounterContract() {
       if (!counterContract) return;
       setVal(null);
       const val = await counterContract.getCounter();
-      setVal(Number(val));
+      setVal(val.toString());
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
     }
     getValue();
   }, [counterContract]);
@@ -29,5 +35,8 @@ export function useCounterContract() {
   return {
     value: val,
     address: counterContract?.address.toString(),
+    sendIncrement: () => {
+      return counterContract?.sendIncrement(sender);
+    },
   };
 }
